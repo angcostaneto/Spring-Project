e20a3e0e0e440e43a6717c6ec14a3f698d364042
Ângelo Costa Neto <angelo_costa_neto@outlook.com>
2021-03-31 23:00:34 -0300

    Correct template style

--- src/main/java/com/alura/mvc/mudi/model/Pedido.java
+++ src/main/java/com/alura/mvc/mudi/model/Pedido.java
@@ -3,7 +3,6 @@ package com.alura.mvc.mudi.model;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
-
 public class Pedido {
     private String nomeProduto;
     private BigDecimal valorNegociado;
